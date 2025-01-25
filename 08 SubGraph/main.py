f8adfac0f924e223d4b50283f861043ee89cab28
Homun Mage <homun@posetmage.com>
2025-01-25 17:21:22 +0800

    todo: update global number

--- 08 SubGraph/main.py
+++ 08 SubGraph/main.py
@@ -6,12 +6,15 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 # ==========================
+# Subgraph Registry
+# ==========================
+subgraph_registry: Dict[str, Any] = {}
+
+# ==========================
 # Common state
 # ==========================
 class SharedState(TypedDict):
-    input: Union[str, None]
     buy_item: Union[str, None]
-
 
 # ==========================
 # Subgraph: Lottery
@@ -20,14 +23,13 @@ class LotteryState(TypedDict):
     input: str
     winnings: Union[str, None]
     missed: Union[str, None]
-
+    try_times: int
 
 def BuyLottery(state: LotteryState):
     random_number = random.randint(0, 2)
     print("buy number: " + str(random_number))
     state['input'] = random_number
     return state
-
 
 def Checking(state: LotteryState):
     prize_number = random.randint(0, 2)
@@ -47,7 +49,6 @@ def checking_result(state: LotteryState) -> Literal["win", "missed"]:
         print("You missed. Buy again.")
         return "missed"
 
-
 lottery_builder = StateGraph(LotteryState)
 lottery_builder.add_node("buy", BuyLottery)
 lottery_builder.add_node("check", Checking)
@@ -62,6 +63,7 @@ lottery_builder.add_conditional_edges(
     },
 )
 lottery_graph = lottery_builder.compile()
+subgraph_registry["lottery"] = lottery_graph
 
 
 # ==========================
@@ -79,113 +81,63 @@ buy_builder.add_node("buy", buy_ice_cream)
 buy_builder.set_entry_point("buy")
 buy_graph = buy_builder.compile()
 
-# ==========================
-# Subgraph Registry
-# ==========================
-subgraph_registry: Dict[str, Any] = {
-    "lottery": lottery_graph,
-    "buy": buy_graph,
-}
+subgraph_registry["buy"] = buy_graph
+
 
 # ==========================
 # Subgraph: Root
 # ==========================
 class RootState(TypedDict):
-    active_subgraph: Union[Literal["lottery", "buy"], None]
-    input: Union[str, None]
     buy_item: Union[str, None]
-
-def route_to_subgraph(state: RootState):
-
-    active_subgraph = state.get('active_subgraph')
-
-    if not active_subgraph:
-        return {"active_subgraph": "lottery", "input": None, "buy_item": None}
-
-    if active_subgraph not in subgraph_registry:
-        return {"active_subgraph": None, "input": None, "buy_item": None}
-
-
-    subgraph = subgraph_registry[active_subgraph]
     
-    if active_subgraph == "lottery":
-        response = subgraph.invoke({"input": state['input'], "winnings": None, "missed": None})
-        if response.get("winnings") == "win":
-             return {"active_subgraph": "buy", "input": None,  "buy_item": None}
-        else:
-             return {"active_subgraph": "lottery", "input": response['input'], "buy_item": None}
-    
-    elif active_subgraph == "buy":
-        response = subgraph.invoke({"buy_item": state['buy_item']})
-        return {"active_subgraph": None, "input": None,  "buy_item": response['buy_item']}
+def route_to_lottery(state: RootState):
+    subgraph = subgraph_registry["lottery"]
+    response = subgraph.invoke({"input": "0", "winnings": None, "missed": None, "try_times": 0})
+    return {"buy_item": state["buy_item"], "lottery_result": response.get("winnings")}
 
-    else:
-        return {"active_subgraph": None, "input": None, "buy_item": None}
-
-
-def check_subgraph_end(state: RootState) -> Literal["continue", "end"]:
-    if state.get("active_subgraph") == None:
-        return "end"
-    else:
-        return "continue"
-
+def route_to_buy(state: RootState):
+    subgraph = subgraph_registry["buy"]
+    response = subgraph.invoke({"buy_item": state['buy_item']})
+    return {"buy_item": response.get("buy_item")}
 
 root_builder = StateGraph(RootState)
-root_builder.add_node("route", route_to_subgraph)
-root_builder.set_entry_point("route")
-root_builder.add_conditional_edges("route", check_subgraph_end, {
-    "continue": "route",
-    "end": END
-})
+root_builder.add_node("lottery", route_to_lottery)
+root_builder.add_node("buy", route_to_buy)
+root_builder.set_entry_point("lottery")
+root_builder.add_edge("lottery", "buy")
 root_graph = root_builder.compile()
 
+subgraph_registry["root"] = root_graph
 
 # ==========================
 # Main Graph: Top Level
 # ==========================
 class MainGraphState(TypedDict):
-    input: Union[str, None]
     buy_item: Union[str, None]
 
-def main_entry(state: MainGraphState):
-    return {"input": None,  "buy_item": None}
 
-
-def invoke_root(state: MainGraphState):
-    response = root_graph.invoke(
-         {"active_subgraph": None, "input": state['input'],  "buy_item": state['buy_item']}
+def invoke_subgraph(state: MainGraphState):
+    subgraph = subgraph_registry["root"]
+    response = subgraph.invoke(
+         {"buy_item": state['buy_item']}
     )
-
     return  {
-             "input": response.get("input"),
              "buy_item": response.get("buy_item")
     }
     
 
-def check_main_end(state: MainGraphState) -> Literal["continue", "end"]:
-    if state.get("buy_item") == "ice cream":
-        return "end"
-    else:
-        return "continue"
+main_graph = StateGraph(MainGraphState)
+main_graph.add_node("subgraph", invoke_subgraph)
+main_graph.set_entry_point("subgraph")
 
+main_graph = main_graph.compile()
 
-main_builder = StateGraph(MainGraphState)
-main_builder.add_node("main_entry", main_entry)
-main_builder.add_node("root", invoke_root)
-main_builder.set_entry_point("main_entry")
-main_builder.add_edge("main_entry", "root")
-main_builder.add_conditional_edges("root", check_main_end, {
-    "continue": "root",
-    "end": END
-})
-main_graph = main_builder.compile()
 
 # ==========================
 # Run
 # ==========================
 for s in main_graph.stream(
     {
-        "input": None,
         "buy_item": None,
     }
 ):
